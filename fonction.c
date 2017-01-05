9c2e80d02f8d02970a89d750fe0663b6b435eff3
Gaut-vador <gauthier.pirlet@gmail.com>
2017-01-05 10:43:02 +0100

    save

--- fonction.c
+++ fonction.c
@@ -6,7 +6,20 @@ void execute_ligne_commande(void){
 	int *pNb = &nb;
 
 	char ***commande = ligne_commande(pFlag, pNb);
-	//affiche(commande);
+
+    while(flag == 1){
+    int pid = fork();
+
+      if(pid == 0){
+        execvp(commande[0][0], commande[0]);
+        perror("execvp");
+        exit(EXIT_FAILURE);
+      }
+      else if(pid != -1){
+        afficher_prompt();
+        commande = ligne_commande(pFlag, pNb);
+      }
+  }
 
 	if(flag == 0){
 		pid_t id = fork();
@@ -21,10 +34,10 @@ void execute_ligne_commande(void){
         exit(EXIT_FAILURE);
       }
       fflush(stdout);
-			//waitpid(id, int *status, int options);
 		}
 	}
 }
+
 
 void afficher_prompt(void){
   
@@ -59,4 +72,28 @@ void addStr(char *chaine, int n, char *directory, int len) {
   for(i = 0 ; i < len ; i++){
     chaine[i+n] = directory[i];
   }
+}
+
+int lance_commande( int in, int out, char *com, char ** argv){
+  int pid = fork();
+  
+  if(pid == -1){
+    perror("fork");
+    return -1;
+  }
+
+  if(pid == 0){
+    if(in != 0){
+      close(0);
+      dup(in);
+    }
+    if(out != 1){
+      close(1);
+      dup(out);
+    }
+    execvp(com, argv);
+    perror("Ligne de commande");
+    return -1;
+  }
+  return pid;
 }
--- fonction.h
+++ fonction.h
@@ -1,3 +1,5 @@
 void execute_ligne_commande(void);
 void afficher_prompt(void);
 void addStr(char *chaine, int n, char *directory, int len);
+
+int lance_commande( int in, int out, char *com, char ** argv);
